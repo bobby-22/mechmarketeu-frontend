9d51cd58cc6287590d32ada2add7fb5c63d8e4ba
bobby-22 <nlongn22@icloud.com>
2021-09-20 00:06:57 +0200

    Fix

--- babel.config.js
+++ babel.config.js
@@ -5,5 +5,4 @@ if (process.env.NODE_ENV === "production") {
 module.exports = {
     presets: ["@vue/cli-plugin-babel/preset"],
     plugins: removeConsolePlugin,
-    productionSourceMap: false,
 };
